1c03a541c1ed4f47c72ea2483fd2a5937466d8a8
Ryota Horii <horii.ryota@sharp.co.jp>
2023-05-16 13:06:29 +0900

    Add event controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'site#index'
+
+  namespace :api do
+    resources :events, only: %i[index show create destroy update]
+  end
 end
